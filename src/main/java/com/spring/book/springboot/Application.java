a23d8dbf6e594e80dad7b49cbfb705a00b8d52c7
seulgi7 <seulgi7707@gmail.com>
2023-01-18 05:42:55 +0900

    almost completed chpter02 code

---
+++ src/main/java/com/spring/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.spring.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/* Application : 프로젝트의 메인 클래스 */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/test/java/com/spring/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,40 @@
+package com.spring.book.springboot.web;
+
+import com.spring.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void return_hello() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void return_helloDto(){
+        String name = "hello";
+        int amount = 1000;
+
+        /*mvc.perform(get("/hello/dti").param("name",name).param("amount",amount))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))*/
+
+    }
+}
